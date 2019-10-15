fb6b1c04489db93c55e3b2eca17039eaeec5dd7b
Joyce Wang <xwan8513@uni.sydney.edu.au>
2019-10-15 15:42:50 +1100

    Copy file from docker container and remote machine

--- rundocker.sh
+++ rundocker.sh
@@ -8,3 +8,10 @@ docker run --name pprl -t -m 8g pprlblock
 
 # remove this container once it is done
 docker container rm --force pprl
+
+
+# copy output file to host system
+docker cp pprl:/result_n=461167.csv .
+
+# copy file from remote fractal machine to local
+# scp wan220@fractal-ev:psig/result_n=461167.csv ./    
